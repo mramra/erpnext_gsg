19967b2181d8fbddff2900332405935522570a1f
Mahmoud Ramadan <mramra1980@gmail.com>
2023-03-10 06:37:04 +0000

    4

--- erpnext_gsg/erpnext_gsg/doc_event/employee_event.py
+++ erpnext_gsg/erpnext_gsg/doc_event/employee_event.py
@@ -8,5 +8,9 @@ def hour(from_time,to_time):
 
 @frappe.whitelist()
 def get_salary(name):
-     return 10
+    salery=frappe.db.sql("""select rounded_total , max(creation) from `tabSalary Slip` WHERE `employee`=%s""",name,as_dict=1)
+    if salery[0].rounded_total:
+        return salery[0].rounded_total
+    else:
+        return 0
 
